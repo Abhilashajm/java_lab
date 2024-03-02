f2c26f4459adde52341e41db84b18f299773c19d
Abhilashajm <161195487+Abhilashajm@users.noreply.github.com>
2024-03-02 20:01:17 +0530

    Update Negativenumbers.java

--- Negativenumbers.java
+++ Negativenumbers.java
@@ -2,27 +2,23 @@ package week5;
 import java.util.Scanner;
 public class Negativenumbers 
 {
-
-	    public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 	        System.out.println("Enter the number of elements: ");
 	        int n = sc.nextInt();
 	        System.out.println("Enter the array elements: ");
 	        int[] arr = new int[n];
-	        
-	        for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 	            arr[i] = sc.nextInt();
-	        }
-	        
-	        int sum = 0;
+		}
+		int sum = 0;
 	        for (int i = 0; i < n; i++) {
 	            if (arr[i] < 0) {
 	                sum += arr[i];
 	            }
-	        }
-	        
-	        System.out.println("The sum of negative numbers = " + sum);
+		}
+		System.out.println("The sum of negative numbers = " + sum);
 	        sc.close();
-	    }
 	}
+}
 	
