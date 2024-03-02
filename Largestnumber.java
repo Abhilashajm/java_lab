1dfbc81b75dc23e945db4255d2f9715c3edb1572
Abhilashajm <161195487+Abhilashajm@users.noreply.github.com>
2024-03-02 18:12:29 +0530

    Add files via upload

--- Largestnumber.java
+++ Largestnumber.java
@@ -1,31 +1,33 @@
-
-
 package week5;
-import java.util.Scanner;
-public class Largestnumber{
-	public static void main(String[] args) {
-		Scanner cs=new Scanner (System.in);
-		System.out.println("enter first number");
-		int num1=cs.nextInt();
-		System.out.println("enter second number"); 
-		int num2=cs.nextInt();
-		System.out.println("enter third number");
-		int num3=cs.nextInt();
-		int largest;
-		if (num1>num2)
-		{
-			if (num1>num3)
-		         	largest=num1;
-			else
-		         	largest=num3;
+import java.util.*;
+public class Largestnumber 
+{
+
+		public static void main(String[]args) {
+			Scanner cs=new Scanner(System.in);
+			System.out.println("Enter the first number:");
+			int num1=cs.nextInt();
+			System.out.println("Enter the second number:");
+			int num2=cs.nextInt();
+			System.out.println("Enter the third number:");
+			int num3=cs.nextInt();
+			int largest;
+			if(num1>num2) {
+				if(num1>num3)
+					largest=num1;
+				else
+					largest=num3;
+			}
+			else {
+				if(num2>num3)
+					largest=num2;
+				else
+					largest=num3;
+			}
+			System.out.println("The Largest number is "+largest);
+
 		}
-		else {
-			if (num2>num3)
-				largest=num2;
-			else
-				largest=num3;
-		}
-		System.out.println("the largest number is "+largest);
+
 	}
-}
-			
+
+
